56ffaaa396e9171bf764db119b2cf0daa59cffa2
jin <yjj1544@gmail.com>
2026-04-29 14:28:53 +0900

    refactor: 필드명이 다른 속성의 매핑을 위해 애너테이션 추가

--- src/main/java/org/pgsg/product/application/mapper/ProductApplicationMapper.java
+++ src/main/java/org/pgsg/product/application/mapper/ProductApplicationMapper.java
@@ -1,6 +1,7 @@
 package org.pgsg.product.application.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.pgsg.product.application.dto.info.ProductInfo;
 import org.pgsg.product.application.dto.result.FindProductResult;
 import org.pgsg.product.domain.model.Product;
@@ -8,6 +9,7 @@ import org.pgsg.product.domain.model.Product;
 @Mapper(componentModel = "spring")
 public interface ProductApplicationMapper {
 	//entity -> dto
+	@Mapping(source = "id", target = "productId")
 	ProductInfo toInfoResult(Product product);
 	FindProductResult toFindResult(Product product);
 }
