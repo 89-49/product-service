7a3372ba801d14427fc57d06aa626d7e93c5d523
jin <yjj1544@gmail.com>
2026-04-30 18:19:36 +0900

    test: 예외 검증 테스트 진행 - 빌드 중 발견된 부분 수정

--- src/test/java/org/pgsg/product/global/exception/ProductErrorCodeTest.java
+++ src/test/java/org/pgsg/product/global/exception/ProductErrorCodeTest.java
@@ -2,32 +2,30 @@ package org.pgsg.product.global.exception;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 
 class ProductErrorCodeTest {
 	@Test
 	void validateYamlErrorKeys() throws Exception {
-		// 1. YAML 파일 로드 (application-product-error.yml 파일명에 맞게 수정하세요)
-		YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
-		List<PropertySource<?>> productErrorYaml = loader.load("product-error", new ClassPathResource("application-product-error.yml"));
-		List<PropertySource<?>> commonErrorYaml = loader.load("common-error", new ClassPathResource("application-error.yml"));
+		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+		factory.setResources(new ClassPathResource("application-product-error.yml"));
+		Properties props = factory.getObject();
 
-		// 2. Enum의 모든 키 가져오기
-		// getErrorKey()가 "product.resource.not-found.product" 같은 문자열을 반환한다고 가정
 		for (ProductErrorCode exception : ProductErrorCode.values()) {
-			String expectedKey = "error.configs." + exception.getErrorKey();
+			String expectedKey = "error.configs[" + exception.getErrorKey() + "].code";
 
-			// 3. YAML에 해당 키가 존재하는지 검증
-			boolean existsInProduct = productErrorYaml.stream().anyMatch(ps -> ps.containsProperty(expectedKey));
-			boolean existsInCommon = commonErrorYaml.stream().anyMatch(ps -> ps.containsProperty(expectedKey));
-
-			assertThat(existsInProduct || existsInCommon)
-				.withFailMessage("YAML 파일에 정의되지 않은 에러 키가 존재합니다: " + expectedKey)
+			assertThat(props.containsKey(expectedKey))
+				.withFailMessage("YAML 파일에 정의되지 않은 에러 키: %s", expectedKey)
 				.isTrue();
 		}
 	}
